525565c05367b55fe425c1a5f0df1407701ddbf2
weiyutang1010 <weiyutang1010@outlook.com>
2020-08-06 10:54:19 -0700

    8/6 Leetcode daily challenge

---
+++ LeetCode/0442/Find All Duplicates in an Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        
+        for (int i = 0; i < (int)nums.size(); i++) {
+            if (nums[abs(nums[i]) - 1] < 0) {
+                ans.push_back(abs(nums[i]));
+            }
+            nums[abs(nums[i]) - 1] *= -1;
+        }
+        
+        return ans;
+    }
+};
