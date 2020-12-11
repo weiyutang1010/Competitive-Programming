7fb3a1be98739f32b2f60eb7a28037c810bcf66c
weiyutang1010 <weiyutang1010@outlook.com>
2020-12-10 17:40:02 -0800

    optimized code

--- LeetCode/0673/Number of Longest Increasing Subsequence.cpp
+++ LeetCode/0673/Number of Longest Increasing Subsequence.cpp
@@ -6,23 +6,21 @@ class Solution {
 public:
     int findNumberOfLIS(vector<int>& v) {
         // dp pair contains { longest increasing length, number of path }
-        vector<ii> dp (v.size(), make_pair(1, 0));
-        int curr_max = 0; 
+        vector<ii> dp (v.size(), make_pair(1, 1));
+        int curr_max = 1; 
         for (int i = 1; i < v.size(); i++) {
-            for (int j = 0; j < i; j++) {
+            for (int j = i - 1; j >= 0; j--) {
                 if (v[j] < v[i]) {
-                    dp[i].f = max(dp[i].f, dp[j].f + 1);
-                    curr_max = max(curr_max, dp[i].f);
-                }
-            }
-            
-            for (int j = 0; j < i; j++) {
-                if (v[j] < v[i] && dp[j].f + 1 == dp[i].f) {
-                    dp[i].s += (dp[j].s == 0 ? 1 : dp[j].s);
+                    if (dp[j].f + 1 > dp[i].f) {
+                        dp[i].f = dp[j].f + 1;
+                        dp[i].s = dp[j].s;
+                        curr_max = max(curr_max, dp[i].f);
+                    } else if (dp[j].f + 1 == dp[i].f) {
+                        dp[i].s += dp[j].s;
+                    }
                 }
             }
         }
-
         
         int ans = 0;
         for (auto x: dp) {
@@ -31,6 +29,6 @@ public:
             }
         }
         
-        return (ans == 0 ? v.size() : ans);
+        return ans;
     }
 };
