45266cb7239df802d1a5563b0efb051db28f7ebe
weiyutang1010 <weiyutang1010@outlook.com>
2021-04-05 14:16:40 +0800

    Changed filename

--- BinarySearch/0983 First Fit Room.cpp
+++
@@ -1,9 +0,0 @@
-int solve(vector<int>& rooms, int target) {
-    int n = rooms.size();
-    for (int i = 0; i < n; i++) {
-        if (rooms[i] >= target) {
-            return rooms[i];
-        }
-    }
-    return -1;
-}
